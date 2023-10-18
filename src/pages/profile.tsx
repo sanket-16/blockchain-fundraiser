02e47ad3e4db2fce4a1d9576b34ccd7b6af94b13
Nidhi Patil <nidhipatil1802@gmail.com>
2023-10-18 12:57:43 +0530

    profile pg

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,3 +1,4 @@
+import CampaignCard from "@/components/CampaignCard";
 import React, { useState } from "react";
 
 const profile = () => {
@@ -45,18 +46,63 @@ const ProfileTabs = () => {
           <button
             key={index}
             onClick={() => setTab(tabVal)}
-            className={`${
-              tabVal === tab && "border-t-2 py-10  font-bold text-foreground"
-            }`}
+            className={`${tabVal === tab && "border-t-2 py-10  font-bold text-foreground"
+              }`}
           >
             {tabVal}
           </button>
         ))}
       </div>
       <div>
-        {tab === tabs[0] && <div>My transactions</div>}
-        {tab === tabs[1] && <div>Wishlist</div>}
-        {tab === tabs[2] && <div>My campaign</div>}
+        {/* for transaction tab */}
+        {tab === tabs[0] && <div>
+          <div className="flex flex-col p-4">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div className="overflow-hidden">
+                  <table className="min-w-full text-sm font-light text-center">
+                    <thead className="border-b font-medium dark:border-neutral-500">
+                      <tr>
+                        <th scope="col" className="px-6 py-4">Invoice</th>
+                        <th scope="col" className="px-6 py-4">Status</th>
+                        <th scope="col" className="px-6 py-4">Method</th>
+                        <th scope="col" className="px-6 py-4">Amount</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr className="border-b dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                        <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                        <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                        <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                      </tr>
+                      <tr className="border-b dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                        <td className="whitespace-nowrap px-6 py-4">Jacob</td>
+                        <td className="whitespace-nowrap px-6 py-4">Thornton</td>
+                        <td className="whitespace-nowrap px-6 py-4">@fat</td>
+                      </tr>
+                      <tr className="border-b dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                        <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                        <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                        <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>}
+        {/* for the wishlist detail */}
+        {tab === tabs[1] && <div>
+          <CampaignCard/>
+            </div>}
+          {/* for the my campaign detail */}
+        {tab === tabs[2] && <div>
+          <CampaignCard/>
+          </div>}
       </div>
     </div>
   );
